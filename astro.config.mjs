040c8a72c95b420c27e8f47b3fe25bea6ce1e47d
Dylan Hand <dylan.hand@gmail.com>
2024-01-31 20:39:52 +1000

    Add preact greeting to main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-bloggy-boy.netlify.app/"
+  site: "https://astro-bloggy-boy.netlify.app/",
+  integrations: [preact()]
 });
