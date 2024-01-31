31c5df672699014d940ad61a130b0f27483c849c
Dylan Hand <dylan.hand@gmail.com>
2024-01-31 20:06:43 +1000

    Fix Typescript errors
    Though dev server was running fine, there were typescript errors when running `npm run build`

---
+++ src/types/frontmatter.ts
@@ -0,0 +1,13 @@
+export interface Frontmatter {
+    layout: string;
+    title: string;
+    pubDate: string;
+    description?: string;
+    author?: string;
+    image: {
+      url: string;
+      alt: string;
+    };
+    tags?: string[];
+  }
+  
